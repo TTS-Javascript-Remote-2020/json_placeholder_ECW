d19b28bf3347cce4f90cab286546548ac02e92b2
Elliott Woodward <elliottcwoodward@gmail.com>
2020-05-31 11:25:23 -0400

    Build out promises

--- main.js
+++ main.js
@@ -5,6 +5,93 @@ function clear() {
   $("#response").html("");
 }
 
+function handleUsernameError(error) {
+  console.warn(error, "sending sample User");
+  return {
+    id: 9999,
+    name: "Elliott Woodward",
+    username: "Sample User",
+  };
+}
+
+function handleGetPostsError(error) {
+  console.warn(error, "sending sample Posts");
+  return [
+    {
+      userId: 9999,
+      id: 9999,
+      title: "Sample Post One",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, eligendi quisquam sit nam unde reiciendis cumque fuga aut explicabo nobis molestiae voluptate et non aliquam eius. Illo nulla eaque optio?",
+    },
+    {
+      userId: 9999,
+      id: 9998,
+      title: "Sample Post Two",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, eligendi quisquam sit nam unde reiciendis cumque fuga aut explicabo nobis molestiae voluptate et non aliquam eius. Illo nulla eaque optio?",
+    },
+  ];
+}
+
+function handleGetAlbumsError(error) {
+  console.warn(error, "sending sample Albums");
+  return [
+    {
+      userId: 9999,
+      id: 9999,
+      title: "Sample Album One",
+    },
+    {
+      userId: 9999,
+      id: 9998,
+      title: "Sample Album Two",
+    },
+  ];
+}
+
+function handleCommentsError(error) {
+  console.warn(error, "sending sample Comments");
+  return [
+    {
+      postId: 9999,
+      id: 9999,
+      name: "John Doe",
+      email: "john.doe@json.api",
+      body:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit esse amet iusto aliquid, corrupti laborum? Explicabo, deserunt! Possimus, tenetur quod?",
+    },
+    {
+      postId: 9999,
+      id: 9998,
+      name: "Jane Doe",
+      email: "jane.doe@json.api",
+      body:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit esse amet iusto aliquid, corrupti laborum? Explicabo, deserunt! Possimus, tenetur quod?",
+    },
+  ];
+}
+
+function handleGetPhotosError(error) {
+  console.warn(error, "sending sample Photos");
+  return [
+    {
+      albumId: 9999,
+      id: 9999,
+      title: "Sample Photo One",
+      url: "",
+      thumbnailUrl: "",
+    },
+    {
+      albumId: 9999,
+      id: 9998,
+      title: "Sample Photo Two",
+      url: "",
+      thumbnailUrl: "",
+    },
+  ];
+}
+
 function getUserByName(username) {
   return new Promise(function (resolve, reject) {
     $.get(url + "users?username=" + username, function (users) {
@@ -32,22 +119,16 @@ function getPostsByUser(user) {
   });
 }
 
-function handleUsernameError(error) {
-  console.warn(error, "using id=1 instead");
-  return {
-    username: "Sample User",
-    id: 9999,
-  };
-}
-
-function handleGetPostError(error) {
-  console.warn(error);
-  return {
-    id: 9999,
-    title: "Sample Post",
-    body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, eligendi quisquam sit nam unde reiciendis cumque fuga aut explicabo nobis molestiae voluptate et non aliquam eius. Illo nulla eaque optio?",
-  };
+function getAlbumsByUser(user) {
+  return new Promise(function (resolve, reject) {
+    $.get(url + "users/" + user.id + "/albums", function (albums) {
+      if (albums.length) {
+        resolve(albums);
+      } else {
+        reject("No albums found for userId: " + user.id);
+      }
+    });
+  });
 }
 
 function getPostByID(postID) {
@@ -58,43 +139,32 @@ function getPostByID(postID) {
   });
 }
 
-function login() {
-  const username = $("#username").val();
-  getUserByName(username)
-    .catch(handleUsernameError)
-    .then(getPostsByUser)
-    .then(renderHomePage);
+function getAlbumByID(albumID) {
+  return new Promise(function (resolve, reject) {
+    $.get(url + "albums/" + albumID, function (album) {
+      resolve(album);
+    });
+  });
 }
 
-function renderPost(post) {
-  // console.log(post);
-  $("h1").text(post.title);
-  clear();
-  const body = "<p class='card'>" + post.body + "</p>";
-  $("#response").append(body);
+function getPhotosByAlbumID(albumID) {
   return new Promise(function (resolve, reject) {
-    resolve(post.id);
+    $.get(url + "albums/" + albumID + "/photos", function (photos) {
+      resolve(photos);
+    });
   });
 }
 
 function getComments(postID) {
   return new Promise(function (resolve, reject) {
     $.get(url + "comments?postId=" + postID, function (comments) {
-      resolve(comments);
+      if (comments.length) {
+        resolve(comments);
+      } else {
+        reject("No comments found for postId: " + postID);
+      }
     });
   });
-}
-
-function renderComments(comments) {
-  console.log(comments);
-}
-
-function renderPostPage(postID) {
-  getPostByID(postID)
-    .catch(handleGetPostError)
-    .then(renderPost)
-    .then(getComments)
-    .then(renderComments);
 }
 
 function renderHomePage([user, posts]) {
@@ -122,6 +192,49 @@ function renderHomePage([user, posts]) {
       event.preventDefault();
     });
   });
+}
+
+function renderPostPage(postID) {
+  getPostByID(postID)
+    .catch(handleGetPostsError)
+    .then(renderPost)
+    .then(getComments)
+    .then(renderComments);
+}
+
+function renderAlbumPage(albumID) {
+  const albumPromise = getAlbumByID(albumID);
+  const photosPromise = getPhotosByAlbumID(albumID);
+  Promise.all([albumPromise, photosPromise]).then(funtion (results) {
+    renderAlbum(results);
+  });
+}
+
+function renderPost(post) {
+  // console.log(post);
+  $("h1").text(post.title);
+  clear();
+  const body = "<p class='card'>" + post.body + "</p>";
+  $("#response").append(body);
+  return new Promise(function (resolve, reject) {
+    resolve(post.id);
+  });
+}
+
+function renderAlbum([album, photos]) {
+  $("h1").text(album.title);
+}
+
+function renderComments(comments) {
+  console.log(comments);
+}
+
+function login() {
+  const username = $("#username").val();
+  getUserByName(username)
+    .catch(handleUsernameError)
+    .then(getPostsByUser)
+    .then(renderHomePage);
 }
 
 $(document).ready(function () {
